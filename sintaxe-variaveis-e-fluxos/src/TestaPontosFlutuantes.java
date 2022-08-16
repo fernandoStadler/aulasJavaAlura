1621c543406903a1acc3aeb34f25914a8bbd9131
Fernando Stadler <fernandostadler2@gmail.com>
2022-08-16 19:44:43 -0300

    first commit

---
+++ sintaxe-variaveis-e-fluxos/src/TestaPontosFlutuantes.java
@@ -0,0 +1,21 @@
+
+public class TestaPontosFlutuantes {
+
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println(salario);
+		
+		double primeiroValor;
+		double segundoValor;
+		double resultadoDivisao;
+		
+		primeiroValor = 3.14;
+		segundoValor = 2;
+		
+		resultadoDivisao = primeiroValor/segundoValor;
+		
+		System.out.println(resultadoDivisao);
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxos/src/TestaVariaveis.java
@@ -0,0 +1,14 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		
+		System.out.println("Olá mundo");
+
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println("A idade é " + idade + " parabéns");
+	}
+		
+}
